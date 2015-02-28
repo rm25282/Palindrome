754876c90c8f29f4660588cca78832c25fc08d88
rm25282 <richardjmartin@gmail.com>
2015-02-28 22:50:33 +0000

    Updated for TCP server

--- InMemoryStore/src/main/java/com/richard/memorystore/rest/Application.java
+++ InMemoryStore/src/main/java/com/richard/memorystore/rest/Application.java
@@ -4,11 +4,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.richard.memorystore.tcp.TcpServer;
+
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        
+        TcpServer.main();
     }
 }
---
+++ InMemoryStore/src/main/java/com/richard/memorystore/tcp/SimpleGateway.java
@@ -0,0 +1,7 @@
+package com.richard.memorystore.tcp;
+
+public interface SimpleGateway {
+
+	public String send(String text);
+
+}
